adf66273e67c1fd9fd33bbb04d5ff56fb18cd456
Tim Morello <34800427+tdmorello@users.noreply.github.com>
2022-11-10 09:25:09 -0500

    Update writer function

--- src/napari_geojson/_writer.py
+++ src/napari_geojson/_writer.py
@@ -5,8 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, Dict, List, Sequence, Tuple, Union
 
 import geojson
-from geojson.feature import FeatureCollection
-from geojson.geometry import GeometryCollection, LineString, Polygon
+from geojson.geometry import LineString, MultiPoint, Polygon
 from napari.layers.shapes._shapes_models import Ellipse
 
 if TYPE_CHECKING:
@@ -14,21 +13,39 @@ if TYPE_CHECKING:
     FullLayerData = Tuple[DataType, dict, str]
 
 
-def write_shapes(path: str, data: Any, meta: dict) -> str:
+def write_shapes(path: str, layer_data: List[Tuple[Any, Dict, str]]) -> str:
     """Write a single geojson file from napari shape layer data."""
     with open(path, "w") as fp:
-        geojson.dump(napari_to_geojson(data, meta), fp)
+        shapes = []
+        for layer in layer_data:
+            data, meta, kind = layer
+            if kind == "points":
+                shapes.append(MultiPoint([list(p) for p in data]))
+            else:
+                shapes.extend(
+                    [
+                        get_geometry(s.tolist(), t)
+                        for s, t in zip(data, meta["shape_type"])  # noqa E501
+                    ]
+                )
+
+        # convert shapes into QuPath friendly format
+        shapes = [format_qupath(s) for s in shapes]
+
+        geojson.dump(shapes, fp)
         return fp.name
 
 
-def napari_to_geojson(data: List, meta: Dict) -> FeatureCollection:
-    """Create a geojson feature collection from a napari shapes layer."""
-    return GeometryCollection(
-        [
-            get_geometry(s.tolist(), t)
-            for s, t in zip(data, meta["shape_type"])  # noqa E501
-        ]
-    )
+def format_qupath(shape, object_type="annotation", is_locked=False):
+    """Convert to QuPath friendly object format."""
+    shape = {
+        "type": "Feature",
+        "geometry": shape,
+        "properties": {"object_type": object_type, "isLocked": is_locked},
+    }
+    if shape["geometry"]["type"] == "Polygon":
+        shape["geometry"]["coordinates"] = [shape["geometry"]["coordinates"]]
+    return shape
 
 
 def get_geometry(coords: List, shape_type: str) -> Union[Polygon, LineString]:
@@ -40,6 +57,11 @@ def get_geometry(coords: List, shape_type: str) -> Union[Polygon, LineString]:
     if shape_type in ["line", "path"]:
         return LineString(coords)
     raise ValueError(f"Shape type `{shape_type}` not supported.")
+
+
+def get_points(coords: List) -> MultiPoint:
+    """Get GeoJSON MultiPoints from napari points layer."""
+    ...
 
 
 def ellipse_to_polygon(coords: List) -> List:
